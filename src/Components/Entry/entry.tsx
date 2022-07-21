6de14554b4d5134511d21b8cd4203f45f6c3b2bf
Muhammad Bilal <bkafridi9833@gmail.com>
2022-07-21 21:43:05 +0500

    Update entry.tsx

--- src/Components/Entry/entry.tsx
+++ src/Components/Entry/entry.tsx
@@ -5,7 +5,6 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { makeStyles } from "@mui/styles";
 import EntryModal from "../EntryModal/entryModal";
 
 interface Props {
